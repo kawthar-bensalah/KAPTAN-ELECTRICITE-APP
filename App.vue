c95bf90e5e40c320227ef690bb61a15ee2092ef7
kawthar-bensalah <68161699+kawthar-bensalah@users.noreply.github.com>
2021-11-29 09:29:44 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container" style="margin-top : 2%; margin-bottom : 2%;">
+        <div id="page" >
+             <img src="src/assets/img/kaptan-electric-logo.png">
+
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<style>
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+</style>
+
+<script>
+
+    export default{
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
